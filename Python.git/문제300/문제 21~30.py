8b98a65707a99f4d59af472356d45b112a9590c9
KeunHee Park <geunii2016@gmail.com>
2021-05-01 16:36:18 +0900

    2021-05-01

---
+++ Python.git/문제300/문제 21~30.py
@@ -0,0 +1,40 @@
+#21번
+print("021번")
+변수1 = 'python'
+print(변수1[0],변수1[2])
+#22번
+print("022번")
+변수2 = "24가 2210"
+print(변수2[3:])
+#23번
+print("023번")
+변수3 = "홀짝홀짝홀짝"
+print(변수3[0::2])
+#24번
+print("024번")
+변수4 = "PYTHON"
+print(변수4[::-1])
+#25번
+print("025번")
+변수5 = "010-1111-2222"
+print(변수5.replace("-"," "))
+#26번
+print("026번")
+print(변수5.replace("-",""))
+#27번
+print("027번")
+변수6 = "http://sharebook.kr"
+print(변수6[-2:])
+#28번
+print("028번")
+print("에러남")
+#29번
+print("029번")
+변수7 = 'abcdfe2a354a32a'
+print(변수7.replace('a','A'))
+#30번
+print("030번")
+print("aBcd")
+
+
+
