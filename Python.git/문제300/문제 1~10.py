a5dc927e762e664404066f36e8e32c375b0c257a
KeunHee Park <geunii2016@gmail.com>
2021-05-01 15:24:02 +0900

    2021-05-01

---
+++ Python.git/문제300/문제 1~10.py
@@ -0,0 +1,34 @@
+#1번
+print("001번")
+print("Hello World")
+#2번
+print("002번")
+print("Mary's cosmetics")
+#3번
+print("003번")
+print("신씨가 소리질렀다",'"도둑이야"')
+#4번
+print("004번")
+print("C:\Windows")
+#5번
+print("005번")
+print("안녕하세요.\n만나서\t\t반갑습니다.")
+print("\엔 은 줄바꿈이고 \티 는 다음 탭으로 넘기기")
+#6번
+print("006번")
+print("오늘은 일요일")
+#7번
+print("007번")
+print("naver","kakao","sk","samsung",sep=';')
+#8번
+print("008번")
+print("naver","kakao","sk","samsung",sep='/')
+#9번
+print("009번")
+print("first",end='')
+print("second")
+#10번
+print("010번")
+print(5/3)
+
+
