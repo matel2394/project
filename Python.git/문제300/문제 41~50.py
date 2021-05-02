996a2636df850436b8b615d9e0ce9c3f6623d011
KeunHee Park <geunii2016@gmail.com>
2021-05-02 14:44:07 +0900

    2021-05-01

---
+++ Python.git/문제300/문제 41~50.py
@@ -0,0 +1,42 @@
+#41번
+print("041번")
+변수1 = "btc_krw"
+print(변수1.upper())
+#42번
+print("042번")
+변수2 = "BTC_KRW"
+print(변수2.lower())
+#43번
+print("043번")
+변수3 = 'hello'
+print(변수3.title())
+#44번
+print("044번")
+변수4 = "보고서.xlsx"
+print(변수4.endswith("xlsx"))
+#45번
+변수4 = "보고서.xlsx"
+print("045번")
+print(변수4.endswith("xlsx" or "xls"))
+#46번
+print("046번")
+변수5 = "2020_보고서.xlsx"
+print(변수5.startswith("2020"))
+#47번
+print("047번")
+변수6 = "hello world"
+print(변수6.split(" "))
+#48번
+print("048번")
+변수7 = "btc_krw"
+print(변수7.split("_"))
+#49번
+print("049번")
+변수8 = "2020-05-01"
+print(변수7.split("-"))
+#50번
+변수9 = "039490   "
+print(변수9.rstrip(),"안녕")
+
+
+
