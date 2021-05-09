b494c4ca923d5a4520966390c44b96bb74cffd68
KeunHee Park <geunii2016@gmail.com>
2021-05-09 15:39:42 +0900

    2021-05-01

---
+++ Python.git/문제300/문제 101~110.py
@@ -0,0 +1,33 @@
+#101번
+print("101번")
+print("bull 타입")
+#102번
+print("102번")
+print("False")
+#103번
+print("103번")
+print("True")
+#104번
+print("104번")
+print("True")
+#105번
+print("105번")
+print("True")
+#106번
+print("106번")
+print(">=가 아닌 =>를 썼기때문")
+#107번
+print("107번")
+print("아무것도 나오지 않는다")
+#108번
+print("108번")
+print("Hi,there.")
+#109번
+print("109번")
+print("1")
+print("2")
+print("4")
+#110번
+print("110번")
+print("3")
+print("5")
