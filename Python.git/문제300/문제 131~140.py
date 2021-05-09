08eda94cd748d31750702a5fe55a930167b4f7e1
KeunHee Park <geunii2016@gmail.com>
2021-05-09 17:38:28 +0900

    2021-05-01

---
+++ Python.git/문제300/문제 131~140.py
@@ -0,0 +1,49 @@
+#131번
+print("131번")
+print("사과")
+print("귤")
+print("수박")
+#132번
+print("132번")
+print("#####")
+print("#####")
+print("#####")
+#133번
+print("133번")
+print("A")
+print("B")
+print("C")
+#134번
+print("134번")
+print("출력A")
+print("출력B")
+print("출력C")
+#135번
+print("135번")
+print("a")
+print("b")
+print("c")
+#136번
+print("136번")
+리스트2 = [10,20,30]
+for 변수 in 리스트2:
+    print(변수)
+#137번
+print("137번")
+리스트2 = [10,20,30]
+for 변수 in 리스트2:
+    print(변수)
+#138번
+print("138번")
+for 변수 in 리스트2:
+    print(변수)
+    print("-------")
+#139번
+print("139번")
+print("++++")
+for 변수 in 리스트2:
+    print("변수")
+#140번
+print("140번")
+for 변수 in range(4):
+    print("-------")
