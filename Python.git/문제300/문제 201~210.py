92471c85ef97bddb5e30cb6b2fc692222c7111d7
KeunHee Park <geunii2016@gmail.com>
2021-05-16 16:28:09 +0900

    2021-05-01

---
+++ Python.git/문제300/문제 201~210.py
@@ -0,0 +1,52 @@
+#201번
+print("201번")
+def print_coin():
+    print("비트코인")
+#202번
+print("202번")
+print_coin()
+#203번
+print("203번")
+for i in range(100):
+    print_coin()
+#204번
+print("204번")
+def print_coin2():
+    for i in range(100):
+        print("비트코인")
+#205번
+print("205번")
+print("hello()함수가 정의 되기 전 호출했기때문에 hello라는 이름을 찾을수 없었다")
+#206번
+print("206번")
+print("A")
+print("B")
+print("C")
+print("A")
+print("B")
+#207번
+print("207번")
+print("A")
+print("C")
+print("B")
+#208번
+print("208번")
+print("A")
+print("C")
+print("B")
+print("E")
+print("D")
+#209번
+print("209번")
+print("B")
+print("A")
+#210번
+print("210번")
+print("B")
+print("C")
+print("B")
+print("C")
+print("B")
+print("C")
+print("A")
+
