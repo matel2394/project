bce6f5269205871e3d76b9cd60370098e095fd03
KeunHee Park <geunii2016@gmail.com>
2021-05-16 17:07:39 +0900

    2021-05-01

---
+++ Python.git/문제300/문제 221~230.py
@@ -0,0 +1,15 @@
+#221번
+print("221번")
+def 리버스(문자열):
+    print(문자열[::-1])
+리버스("python")
+#222번
+print("222번")
+def 성적(a,b,c):
+    print((a+b+c)/3)
+성적(1,2,3)
+#223번
+print("223번")
+def 짝수(a,b,c,d,e,f,g):
+        print(a or b or c or d or e or f or g % 2 ==0)
+짝수(1,2,3,4,5,6,7)
