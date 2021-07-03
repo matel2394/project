b6b9c354aaa2710ac180217aa2bf445d6bd1dd69
matel2394 <83488082+matel2394@users.noreply.github.com>
2021-07-03 15:49:00 +0900

    Add files via upload

--- Python.git/Cos Pro 2/1차시(다시)/1차 2급 6_initial_code.py
+++ Python.git/Cos Pro 2/1차시(다시)/1차 2급 6_initial_code.py
@@ -1,10 +1,10 @@
 def solution(number):
     count = 0
-    for i in range(1, number + 1): #1부터 41
+    for i in range(1, number + 1): #1부터 40
         current = i #1,2,3,4,5...
         temp = count #0
         while current != 0: #current가 0이 아닐때까지 반복
-            if current//3 == 0:
+            if current % 10 == 3 or current % 10 == 6 or current % 10 == 9:
                 count += 1
                 print("pair", end = '')
             current = current // 10
