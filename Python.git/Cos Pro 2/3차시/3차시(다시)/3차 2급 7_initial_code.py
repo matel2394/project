240b77d9072fc79bf2113a873b8a38bd351e6b34
matel2394 <83488082+matel2394@users.noreply.github.com>
2021-07-04 15:16:09 +0900

    Add files via upload

--- Python.git/Cos Pro 2/3차시/3차시(다시)/3차 2급 7_initial_code.py
+++ Python.git/Cos Pro 2/3차시/3차시(다시)/3차 2급 7_initial_code.py
@@ -1,18 +1,19 @@
 def solution(num_apple, num_carrot, k):
     answer = 0 #주스 수
     
-    if num_apple < num_carrot * 3: #사과 수가 당근*3 한것보다 작으면
-        answer = num_apple // 3 #사과를 3으로 나누고 몫만 챙긴 뒤 앤서에 대입
+    if num_apple < num_carrot * 3: #10 < 15
+        answer = num_apple // 3 # answer =3
     else: #아니면
-        answer = num_carrot    #앤서에 당근 수를 대입
+        answer = num_carrot # X   #answer = 1
 
-    num_apple -= answer * 3 #2  # 1
-    num_carrot -= answer #0  #-4
+    num_apple -= answer * 3 # 10 - 9 = 1      #2
+    num_carrot -= answer # 5 - 3 = 2        #0
 
-    i = 0
-    while k - (num_apple + num_carrot + i) > 0: #4 에 -3에 i더해서 뺀 값이 0보다 크면 계속 반복
-        if i % 4 == 0: #만약 0을 4로 나눈것의 나머지가 0이라면
-            answer += 1 #카운트에 1 추가
+    i = 0 #뭔갈 더 빼야하는 갯수를 알려주는 카운터?
+    while k - (num_apple + num_carrot + i) > 0: #4- (3 + i) >0 #여기서 i가 1이 되어야 while문이 종료됨
+        if i % 4 == 0: #0을 4로 나누면 0이 되니까 k - (J+i) = 0을 충족하지 못함
+            answer -= 1 #재료가 하나 빠지니 주스 하나를 더 만들수 없어 1이 빠짐
+            #answer += 1
         i = i + 1 #i = 1 , i=3, i = 5
         
     return answer
