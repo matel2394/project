9f6e8655f95b7aec629bd5cd33b24668d1718787
Dongjunkim <junrah@nate.com>
2021-08-22 16:51:10 +0900

    2021-08-22

--- index/urls.py
+++ index/urls.py
@@ -2,4 +2,7 @@ from django.urls import path,include
 from.import views
 urlpatterns = [
     path('index',views.index),
+    path('footer',views.footer),
+path('login',views.login),
+path('signup',views.signup),
     ]
--- index/views.py
+++ index/views.py
@@ -2,4 +2,10 @@ from django.shortcuts import render
 
 def index(request):
     return render(request,'index.html')
+def footer(request):
+    return render(request,'footer.html')
+def login(request):
+    return render(request,'login.html')
+def signup(request):
+    return render(request,'signup.html')
 # Create your views here.
